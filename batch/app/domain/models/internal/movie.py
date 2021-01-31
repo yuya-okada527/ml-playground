9ee4eceda6dae343abb801d89678a4de850080dd
yuya-okada527 <yuya.okada527@gmail.com>
2021-01-31 19:23:25 +0900

    add movie models

--- batch/app/domain/models/internal/movie.py
+++ batch/app/domain/models/internal/movie.py
@@ -5,3 +5,32 @@ class Genre(BaseModel):
     genre_id: int
     name: str
     japanese_name: str
+
+
+class Review(BaseModel):
+    review_id: str
+    review: str
+
+
+class Keyword(BaseModel):
+    keyword_id: str
+    name: str
+    japanese_name: str
+
+
+class Movie(BaseModel):
+    movie_id: int
+    tmdb_id: str
+    imdb_id: str
+    original_title: str
+    japanese_title: str
+    overview: str
+    tagline: str
+    poster_path: str
+    backdrop_path: str
+    popularity: float
+    vote_average: float
+    vote_count: int
+    reviews: list[Review] = []
+    genres: list[Genre] = []
+    keywords: list[Keyword] = []
--- batch/app/domain/models/rdb/input.py
+++ batch/app/domain/models/rdb/input.py
@@ -5,11 +5,35 @@ from tortoise.models import Model
 class GenreRdbModel(Model):
     genre_id = fields.IntField(pk=True)
     name = fields.CharField(max_length=64)
-    japanese_name = fields.CharField(max_length=64)
+    japanese_name = fields.CharField(max_length=64, null=True)
 
     class Meta:
         table = "genres"
-    
-    def __str__(self) -> str:
-        return f"[{self.genre_id=}, {self.name=}, {self.japanse_name=}]"
 
+
+class MovieGenreRdbModel(Model):
+    movie_id = fields.IntField(pk=True)
+    genre_id = fields.IntField(pk=True)
+
+    class Meta:
+        table = "movie_genres"
+
+
+class MovieRdbModel(Model):
+    movie_id = fields.IntField(pk=True, generated=True)
+    tmdb_id = fields.CharField(max_length=64)
+    # TODO スキーマに誤りがあったため一時コメントアウト
+    # imdb_id = fields.CharField(max_length=64, null=True)
+    original_title = fields.CharField(max_length=256, null=True)
+    japanese_title = fields.CharField(max_length=256, null=True)
+    overview = fields.TextField()
+    tagline = fields.TextField()
+    poster_path = fields.CharField(max_length=256)
+    backdrop_path = fields.CharField(max_length=256, null=True)
+    popularity = fields.FloatField(null=True)
+    vote_average = fields.FloatField(null=True)
+    vote_count = fields.IntField(null=True)
+
+    class Meta:
+        table = "movies"
+    
