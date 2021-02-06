c56fbff84536cf06acff011e78ff04276af63e35
yuya-okada527 <yuya.okada527@gmail.com>
2021-02-06 22:29:09 +0900

    set up material ui for next.js

--- fe/components/Layout.tsx
+++ fe/components/Layout.tsx
@@ -1,41 +1,98 @@
-import React, { ReactNode } from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import React, { ReactNode } from "react";
+import Head from "next/head";
+import AppBar from "@material-ui/core/AppBar";
+import HomeIcon from "@material-ui/icons/Home";
+import InfoIcon from "@material-ui/icons/Info";
+import {
+  Link,
+  Grid,
+  createStyles,
+  makeStyles,
+  Theme,
+  Typography,
+  List,
+  ListItem,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    title: {
+      margin: theme.spacing(2),
+    },
+    offset: {
+      marginTop: theme.spacing(13),
+    },
+    sidebarIcon: {
+      margin: theme.spacing(1),
+    },
+    sidebarText: {
+      marginTop: theme.spacing(0.5),
+    },
+  })
+);
 
 type Props = {
-  children?: ReactNode
-  title?: string
-}
+  children?: ReactNode;
+  title?: string;
+};
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
-)
+const Layout = ({ children, title = "This is the default title" }: Props) => {
+  const classes = useStyles();
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+      </Head>
+      <header>
+        <AppBar>
+          <Typography variant="h4" component="h1" className={classes.title}>
+            Movie Recommender
+          </Typography>
+        </AppBar>
+      </header>
+      <div className={classes.offset} />
+      <Grid container>
+        <Grid item xs={2}>
+          <List>
+            <ListItem>
+              <Link href="/">
+                <Grid container>
+                  <HomeIcon className={classes.sidebarIcon} />
+                  <Typography className={classes.sidebarText} variant="h6">
+                    <a>Home</a>
+                  </Typography>
+                </Grid>
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link href="/about">
+                <Grid container>
+                  <InfoIcon className={classes.sidebarIcon} />
+                  <Typography className={classes.sidebarText} variant="h6">
+                    About
+                  </Typography>
+                </Grid>
+              </Link>
+            </ListItem>
+          </List>
+        </Grid>
+        <Grid item xs={10}>
+          {children}
+        </Grid>
+      </Grid>
 
-export default Layout
+      <footer>
+        <hr />
+        <span>I'm here to stay (Footer)</span>
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
--- fe/pages/index.tsx
+++ fe/pages/index.tsx
@@ -1,15 +1,63 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import React from "react";
+import {
+  Button,
+  Container,
+  createStyles,
+  makeStyles,
+  TextField,
+  Theme,
+  Grid,
+  Typography,
+} from "@material-ui/core";
+import Layout from "../components/Layout";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      marginLeft: theme.spacing(2),
+      marginRight: theme.spacing(2),
+    },
+    searchButton: {
+      marginTop: theme.spacing(1),
+      marginLeft: theme.spacing(2),
+    },
+  })
+);
 
-export default IndexPage
+const IndexPage = () => {
+  const classes = useStyles();
+  return (
+    <Layout title="Movie Recommeder">
+      <Container className={classes.container}>
+        <Grid container>
+          <Grid item xs={8}>
+            <Typography variant="h6" component="h2">
+              Search Your Favorite Movies!!
+            </Typography>
+            <Grid container>
+              <Grid item xs={8}>
+                <TextField
+                  id="search-movie"
+                  label="Search Movies!!"
+                  type="search"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  className={classes.searchButton}
+                >
+                  Search
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
+    </Layout>
+  );
+};
+
+export default IndexPage;
