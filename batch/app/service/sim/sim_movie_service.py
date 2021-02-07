fb6a55c9a9b0346706a99f9778d28631e356ce37
yuya-okada527 <yuya.okada527@gmail.com>
2021-02-07 23:37:28 +0900

    modify similarity batch

--- batch/app/service/sim/sim_movie_service.py
+++ batch/app/service/sim/sim_movie_service.py
@@ -11,20 +11,18 @@ def exec_construct_tmdb_similarity(
 ):
     
     # 出力対象の映画IDを取得
-    movie_id_list = movie_repository.fetch_all_movie_id()
+    movie_id_set = set(movie_repository.fetch_all_movie_id())
 
     # 映画IDごとにKVSデータを構築
-    for movie_id in movie_id_list:
+    for movie_id in movie_id_set:
 
         # 類似映画リストを取得
         similar_movie_list = tmdb_client.fetch_similar_movie_list(movie_id=movie_id)
-
-        # データがない場合スキップ
-        if len(similar_movie_list.results) < 5:
+        if not similar_movie_list:
             continue
 
         # ベスト5のIDを取得
-        best_5 = [movie.id for movie in similar_movie_list.results[:5]]
+        best_5 = [movie.id for movie in similar_movie_list.results if movie.id in movie_id_set][:5]
 
         # KVSデータを構築
         redis_repository.save_movie_similarity(
