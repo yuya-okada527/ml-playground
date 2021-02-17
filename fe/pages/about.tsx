3d546c1cdc8844a1ee15500549621f201214884d
yuya-okada527 <yuya.okada527@gmail.com>
2021-02-17 22:22:23 +0900

    add about page

--- fe/pages/about.tsx
+++ fe/pages/about.tsx
@@ -1,16 +1,27 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { Grid } from "@material-ui/core";
+import Link from "next/link";
+import Layout from "../components/Layout";
 
 const AboutPage = () => (
   <Layout title="About | Next.js + TypeScript Example">
-    <h1>About</h1>
-    <p>This is the about page</p>
-    <p>
-      <Link href="/">
-        <a>Go home</a>
-      </Link>
-    </p>
+    <Grid container>
+      <Grid item xs={8}>
+        <h1>このサイトについて</h1>
+        <p>
+          このサイトは、機械学習を活用したアプリケーションのデモンストレーション環境を提供します。
+        </p>
+        <p>
+          収益化を主目標としていませんが、機械学習を活用したサービスを実環境で継続的に運用・改善していくための工夫を凝らし、設計・開発しています。
+        </p>
+        <p>現在、提供している機能は以下です。</p>
+        <p>
+          <Link href="/">
+            <a>映画推薦</a>
+          </Link>
+        </p>
+      </Grid>
+    </Grid>
   </Layout>
-)
+);
 
-export default AboutPage
+export default AboutPage;
