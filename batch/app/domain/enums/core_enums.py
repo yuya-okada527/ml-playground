f353f66b5f58c7120a5aed2a837339fc13c6d0c8
yuya-okada527 <yuya.okada527@gmail.com>
2021-02-23 22:53:11 +0900

    add enums models

--- batch/app/domain/enums/core_enums.py
+++ batch/app/domain/enums/core_enums.py
@@ -1,6 +1,14 @@
+"""コア区分値モジュール
+
+ドメインに依存しない区分値を定義するモジュール
+"""
 from enum import Enum
 
 
 class LogLevel(Enum):
+    """ログレベルEnumクラス
+
+    ログレベルを定義するenumクラス
+    """
     INFO = "info"
     DEBUG = "debug"
--- batch/app/domain/enums/similarity_enums.py
+++ batch/app/domain/enums/similarity_enums.py
@@ -1,5 +1,13 @@
+"""類似性モジュール
+
+類似性モジュール
+"""
 from enum import Enum
 
 
 class SimilarityModelType(Enum):
+    """類似性モデルタイプ
+
+    類似性モデル区分値
+    """
     TMDB_SIM = "tmdb-sim"
