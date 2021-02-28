7009f6b1cfe87a0e1077821529887988a2b55714
yuya-okada527 <yuya.okada527@gmail.com>
2021-03-01 02:05:30 +0900

    add eslint

--- fe/next.config.js
+++ fe/next.config.js
@@ -1,5 +1,17 @@
+/* eslint-env node */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const path = require("path");
+
 module.exports = {
   images: {
     domains: ["www.themoviedb.org"],
+  },
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      src: path.join(__dirname, "src/"),
+    };
+    return config;
   },
 };
