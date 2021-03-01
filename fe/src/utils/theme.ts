a6689f327247661d421251427e3614e138cccb6f
yuya-okada527 <yuya.okada527@gmail.com>
2021-03-01 21:50:00 +0900

    add jsdocs to utils

--- fe/src/utils/theme.ts
+++ fe/src/utils/theme.ts
@@ -1,8 +1,9 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 import { red } from "@material-ui/core/colors";
 
-// TODO パッケージ構成再考
-// Create a theme instance.
+/**
+ * カスタムテーマ
+ */
 const theme = createMuiTheme({
   typography: {
     fontFamily: [
