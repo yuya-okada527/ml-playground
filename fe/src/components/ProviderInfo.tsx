4d709d9a60834f62753b63aab4cb7e9d8abdd396
yuya-okada527 <yuya.okada527@gmail.com>
2021-03-02 00:17:22 +0900

    modify tmdb logo alt

--- fe/src/components/ProviderInfo.tsx
+++ fe/src/components/ProviderInfo.tsx
@@ -39,7 +39,7 @@ const ProviderInfo: React.FC = () => {
       </Typography>
       <Image
         src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg"
-        alt="Logo of Tmdb"
+        alt="TMDB Logo"
         width={40}
         height={40}
         className={classes.providerIcon}
