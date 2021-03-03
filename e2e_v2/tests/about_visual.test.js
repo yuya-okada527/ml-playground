38e73bcbd2b6cd0925840896c86c5f95bc24fec0
yuya-okada527 <yuya.okada527@gmail.com>
2021-03-03 23:12:38 +0900

    change e2e

--- e2e_v2/tests/about_visual.test.js
+++ e2e_v2/tests/about_visual.test.js
@@ -1,15 +1,14 @@
 const { it, describe, expect } = require("@playwright/test");
 
 const BASE_PAGE = "http://localhost:3000";
-const IMAGE_BASE = "__snapshots__";
 
 describe("Aboutページ ビジュアルテスト", () => {
-  it("スクリーンショット", async ({ page }) => {
+  it("スクリーンショット", async ({ page, browserName }) => {
     await page.goto(`${BASE_PAGE}/about`);
     const screenshot = await page.screenshot({
       fullPage: true,
     });
-    expect(screenshot).toMatchSnapshot("test-about", {
+    expect(screenshot).toMatchSnapshot(`about-${browserName}.png`, {
       threshold: 0.2,
     });
   });
