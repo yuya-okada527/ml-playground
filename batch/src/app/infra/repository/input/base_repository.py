f52bbe93594430d76b6a26776ccd93219ac2df51
yuya-okada527 <yuya.okada527@gmail.com>
2021-03-11 22:54:07 +0900

    add logs

--- batch/src/app/infra/repository/input/base_repository.py
+++ batch/src/app/infra/repository/input/base_repository.py
@@ -2,9 +2,12 @@
 
 リポジトリの共通機能を提供するモジュール
 """
+from batch.src.app.core.logger import create_logger
 from core.config import InputDbSettings
 from sqlalchemy import create_engine
 from sqlalchemy.engine.base import Engine
+
+log = create_logger()
 
 
 def create_input_engine(settings: InputDbSettings) -> Engine:
@@ -12,8 +15,10 @@ def create_input_engine(settings: InputDbSettings) -> Engine:
 
     入稿DBエンジンを作成する
     """
+    url = f"{settings.input_db_engine}://{settings.input_db_user}:{settings.input_db_password}@{settings.input_db_host}:{settings.input_db_port}/{settings.input_db_database}"
+    log.error(url)
     return create_engine(
-        f"{settings.input_db_engine}://{settings.input_db_user}:{settings.input_db_password}@{settings.input_db_host}:{settings.input_db_port}/{settings.input_db_database}"
+
     )
 
 
