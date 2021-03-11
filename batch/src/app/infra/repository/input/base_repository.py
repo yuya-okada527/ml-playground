df5eff9c7b3c01d93f5401b70b29ec8c503ceb00
yuya-okada527 <yuya.okada527@gmail.com>
2021-03-11 23:14:25 +0900

    fix encode

--- batch/src/app/infra/repository/input/base_repository.py
+++ batch/src/app/infra/repository/input/base_repository.py
@@ -3,7 +3,8 @@
 リポジトリの共通機能を提供するモジュール
 """
 import base64
-from batch.src.app.core.logger import create_logger
+
+from app.core.logger import create_logger
 from core.config import InputDbSettings
 from sqlalchemy import create_engine
 from sqlalchemy.engine.base import Engine
@@ -17,7 +18,7 @@ def create_input_engine(settings: InputDbSettings) -> Engine:
     入稿DBエンジンを作成する
     """
     url = f"{settings.input_db_engine}://{settings.input_db_user}:{settings.input_db_password}@{settings.input_db_host}:{settings.input_db_port}/{settings.input_db_database}"
-    log.error(base64.encode(url.encode("utf-8")))
+    log.error(base64.b64encode(url.encode()).decode())
     return create_engine(
 
     )
